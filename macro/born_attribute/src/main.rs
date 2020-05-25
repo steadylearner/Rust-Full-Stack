879c81124954b0ce83b3376bc262b306473d68fb
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-05-25 10:36:18 -0400

    End test with attribute for struct. Decide to not make the same feature for Enum because you can do it with born.

---
+++ macro/born_attribute/src/main.rs
@@ -0,0 +1,14 @@
+#[macro_use] extern crate born_attribute;
+
+#[web_event_base()]
+#[derive(Debug)]
+enum WebEvent {
+    KeyPress(char),
+    Click { x: i64, y: i64 },
+    Paste(String),
+}
+
+fn main() {
+
+}
+
---
+++ macro/born_attribute_dev/src/crate/lib_struct.rs
@@ -0,0 +1,33 @@
+use proc_macro::TokenStream;
+use proc_macro_roids::{
+    FieldsNamedAppend,
+};
+use quote::quote;
+use syn::{parse_macro_input, parse_quote, DeriveInput, FieldsNamed};
+
+macro_rules! struct_fields {
+    (pub $name:ident, { $( $commonfieldpub:vis $commonfield:ident: $commonty:ty ),+ $(,)* }) => {
+        #[proc_macro_attribute]
+        pub fn $name(_args: TokenStream, item: TokenStream) -> TokenStream {
+            let mut ast = parse_macro_input!(item as DeriveInput);
+            let common_fields: FieldsNamed = parse_quote!({ $( $commonfieldpub $commonfield: $commonty, )+ });
+            ast.append_named(common_fields);
+            TokenStream::from(quote! { #ast })
+        }
+    };
+    ($name:ident, { $( $commonfield:ident: $commonty:ty ),+ $(,)* }) => {
+        #[proc_macro_attribute]
+        pub fn $name(_args: TokenStream, item: TokenStream) -> TokenStream {
+            let mut ast = parse_macro_input!(item as DeriveInput);
+            let common_fields: FieldsNamed = parse_quote!({ $( $commonfield: $commonty, )+ });
+            ast.append_named(common_fields);
+            TokenStream::from(quote! { #ast })
+        }
+    };
+}
+
+struct_fields!(pub message_base, {
+    text: String,
+});
+struct_fields!(pub user_base, { pub active: bool, });
+struct_fields!(no_field, { useful: bool, });
